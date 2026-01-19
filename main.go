ccae573cad30345962890ca4f2e03e1c22bf3a35
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2026-01-19 15:01:36 -0800

    impl health check

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/NikkiAung/go-fundmentals/internal/app"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/NikkiAung/go-fundmentals/internal/app"
+)
 
 func main () {
 	app, err := app.NewApplication()
@@ -9,5 +15,25 @@ func main () {
 		panic(err)
 	}
 
-	app.Logger.Println("hello our logger.")
+	app.Logger.Println("Our app is live.")
+
+	s := &http.Server{
+		Addr: ":8080",
+		IdleTimeout: time.Minute,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+
+	// curl localhost:8080/health
+	http.HandleFunc("/health", HealthCheck)
+
+	err = s.ListenAndServe()
+
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is ok\n");
 }

fb05d0d03b2d256b1fa2cd8e3ca6c1c918240c2c
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2026-01-21 18:17:16 -0800

    impls post store

---
+++ internal/store/post_store.go
@@ -0,0 +1,52 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+)
+
+// optional with *
+type Post struct {
+    ID        int64     `json:"id"`
+    Title     string    `json:"title"`
+    Content   string    `json:"content"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+
+type PostStore interface {
+	CreatePost(*Post) (*Post, error)
+	GetPostById(id int) (*Post, error)
+}
+
+type PostgresPostStore struct {
+	db *sql.DB
+}
+
+func NewPostgresPostStore(db *sql.DB) *PostgresPostStore {
+	return &PostgresPostStore{db:db}
+}
+
+func (pg *PostgresPostStore) CreatePost(post *Post) (*Post, error) {
+	tx,err := pg.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	defer tx.Rollback()
+
+	query := "INSERT INTO posts (title, content) VALUES ($1, $2) RETURNING id"
+
+	err = tx.QueryRow(query, post.Title, post.Content).Scan(&post.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = tx.Commit();
+	if err != nil {
+		return nil, err
+	}
+
+	return post, nil
+}
